b178dffcbd4cc59c142fc8e02b2b2b7c761c718f
PIEDRACAR <crubenhp114@gmail.com>
2025-07-07 04:26:23 -0500

    Add files via upload

---
+++ server/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Backend FastAPI funcionando correctamente 🚀"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("server.main:app", host="0.0.0.0", port=port, reload=True)
